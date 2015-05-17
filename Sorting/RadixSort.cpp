8abeafe3c87a26a797ffcfe133c7c87104ccdb12
veeskochill <alex.vyskocil@gmail.com>
2015-05-17 02:23:39 -0400

    overload for pointer-list input

--- Sorting/RadixSort.cpp
+++ Sorting/RadixSort.cpp
@@ -1,15 +1,64 @@
 #include <stdlib.h>
 #include <iostream>
-
 #include <queue>
 #include <array>
+#include <cmath>
 
 
 using namespace std;
 
-//O(k*(n+n))
-queue<int> RadixSort(queue<int> input, int size, int k)
+
+int* RadixSort(int* input, int size, int k = -1)
 {
+	for(int ak=0; ak<k; ak++)
+	{
+		int ik = pow(10,ak);
+			//iterate over each digit
+		array<queue<int>,10> bins;
+
+		//push element into the correct box
+		//based on digit 'ik'
+		for(int ia = 0; ia < size; ia++) //O(n)
+		{
+			int temp = input[ia];
+			bins[(temp /ik) %10].push(temp);
+		}
+
+		//re-create 'input' from bins (Copy)
+		int count = 0;
+		for(int ui =0; ui<10; ui++) 
+		{
+			for(int uj = bins[ui].size();uj>0;uj--) //O(n)
+			{	
+				input[count++] = bins[ui].front();
+				bins[ui].pop();
+
+			}
+		}	
+	}
+	return input;
+}
+
+
+//O(k*(n+n))
+queue<int> RadixSort(queue<int> input, int size, int k = -1)
+{
+	//If the max size is not known, then search for it.
+	if(k <= 0)
+	{
+		int max = input.front();
+		for(int ui =0; ui<size; ui++)
+		{
+			if(input.front() > max)
+				max = input.front();
+			//Recycle to the back of the queue
+			input.push(input.front());
+			input.pop();
+		}
+		//We only need the number of digits.
+		k = ceil(log(k));
+	}
+
 	//iterate over each digit
 	for(int ik = 1; ik < k; ik *=10) //O(K)
 	{
@@ -41,7 +90,7 @@ int main()
 {
 
 	int input[10] = {36, 9, 0, 25, 1 ,49, 64, 16, 81, 4};
-	queue<int> input2;
+	/*queue<int> input2;
 	for(int ui=0;ui<10;ui++)
 		input2.push(input[ui]);
 
@@ -53,6 +102,11 @@ int main()
 		cout << output.front() << endl;
 		output.pop();
 	}
-
+*/
+	int * output = RadixSort(input, 10, 2);
+	for( int ui=0; ui < 10; ui++)
+	{
+		cout << output[ui] << endl;
+	}
 	return 0;
 }
